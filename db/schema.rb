13ac5fc47dbcb85bf79c9b99c111c217e9b2eab3
abosede <thirtyfoura34@gmail.com>
2020-11-25 07:47:07 -0800

    define some models, customize devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_20_232110) do
+ActiveRecord::Schema.define(version: 2020_11_25_151227) do
+
+  create_table "boards", force: :cascade do |t|
+    t.integer "owner_id"
+    t.string "name", default: "", null: false
+    t.string "url", default: "", null: false
+    t.integer "permission", default: 0, null: false
+    t.index ["owner_id"], name: "index_boards_on_owner_id"
+  end
+
+  create_table "boards_users", id: false, force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "board_id", null: false
+    t.index ["board_id"], name: "index_boards_users_on_board_id"
+    t.index ["user_id"], name: "index_boards_users_on_user_id"
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name", default: "", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,8 +39,13 @@ ActiveRecord::Schema.define(version: 2020_11_20_232110) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.integer "team_id"
+    t.integer "role", default: 2, null: false
+    t.integer "approval", default: 2, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "boards", "users", column: "owner_id"
 end
