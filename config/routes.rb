e212462097ceaa227a65fe7f4b525f01fbe2de28
abosede <thirtyfoura34@gmail.com>
2020-11-26 06:59:03 -0800

    revise project

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,3 @@
 Rails.application.routes.draw do
-  root to: 'home#home'
-  devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations',
-  }
-
-  devise_scope :user do
-    get 'sign_in', to: 'users/sessions#new'
-    get 'sign_up', to: 'users/registrations#new'
-    get 'forgot_password', to: 'users/passwords#new'
-    get 'reset_password', to: 'users/passwords#edit'
-  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
