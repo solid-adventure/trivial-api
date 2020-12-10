6df621802df3b76055c97f45016ff403a56fc1ae
abosede <thirtyfoura34@gmail.com>
2020-12-10 13:13:26 -0800

    rspecs init

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   mount_devise_token_auth_for 'User', at: 'auth', controllers: {
     registrations: 'overrides/registrations',
     sessions: 'overrides/sessions'
---
+++ spec/requests/overrides/registration_spec.rb
@@ -0,0 +1,4 @@
+require 'swagger_helper'
+
+RSpec.describe 'overrides/registration', type: :request do
+end
