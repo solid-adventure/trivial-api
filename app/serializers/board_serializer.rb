01d085244ab53c6000d4e1088ba66ac12b1fbb3c
juancarbajal98 <juancarbajal98@gmail.com>
2021-03-17 15:06:09 -0700

    Migration for Boards meta description and featured

--- app/serializers/board_serializer.rb
+++ app/serializers/board_serializer.rb
@@ -1,5 +1,5 @@
 class BoardSerializer < ActiveModel::Serializer
-  attributes  :id, :name, :access_level, :slug, :contents, :description
+  attributes  :id, :name, :access_level, :slug, :contents, :description. :meta_description, :featured
 
   belongs_to :owner
   has_many  :flows
