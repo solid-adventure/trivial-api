b9dba4ad644a369e1678c7bbc40e63609f7e24f4
juancarbajal98 <juancarbajal98@gmail.com>
2021-05-06 17:37:02 -0700

    endpoints

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,6 +1,41 @@
 class WebhooksController < ApplicationController
     def index
+        render json: Webhook.where(app_id: params[:app_id])
+    end
+    
+    def create
+        webhook = Webhook.new(webhook_params)
+        if webhook.save
+            render json: webhook, status: :created
+        else
+            render_bad_request webhook
+        end
+    end
+
+    def show
         webhooks = Webhook.all
         render json: webhooks, include: []
+    end
+
+    def update
+        if webhook.update(webhook_params)
+            render json: webhook
+        else
+            render_bad_request webhook
+        end
+    end
+
+    private
+
+    # def webhooks
+
+    # end
+
+    def webhooks
+        @_webhook ||= Webhook.where(app_id: params[:app_id])
+    end
+
+    def webhook_params
+        params.permit(:app_id, :source)
     end
 end
--- app/models/webhook.rb
+++ app/models/webhook.rb
@@ -1,2 +1,4 @@
 class Webhook < ApplicationRecord
+    validates :app_id, presence: true
+    validates :source, presence: true
 end
