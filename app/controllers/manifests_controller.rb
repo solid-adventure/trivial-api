7a98ea0767247d18a5cfabba34fcaabb726f7423
juancarbajal98 <juancarbajal98@gmail.com>
2021-05-07 16:05:11 -0700

    Working endpoints

---
+++ app/controllers/manifests_controller.rb
@@ -0,0 +1,43 @@
+class ManifestsController < ApplicationController
+    skip_before_action :authenticate_user!, only: [:show, :index, :create, :update]
+
+    def index
+        render json: manifests
+    end
+    
+    def create
+        manifest = Manifest.new(manifest_params)
+        if manifest.save
+            render json: manifest, status: :created
+        else
+            render_bad_request manifest
+        end
+    end
+
+    def show
+        render json: manifest
+    end
+
+    def update
+        if manifest.update(manifest_params)
+            render json: manifest
+        else
+            render_bad_request manifest
+        end
+    end
+
+    private
+
+    def manifest
+        @_manifest ||= Manifest.find(params[:id])
+    end
+
+    def manifests
+        @_manifests ||= Manifest.where(app_id: params[:app_id])
+    end
+
+    def manifest_params
+        params.permit(:app_id, :content, :owner_id)
+    end
+
+end
