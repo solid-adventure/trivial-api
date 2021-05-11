6e77ae7ddbc8a222a12b5b00622290007a4f544d
juancarbajal98 <juancarbajal98@gmail.com>
2021-05-10 17:42:27 -0700

    Validation tests on new Controllers

---
+++ test/models/webhook_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class WebhookTest < ActiveSupport::TestCase
+    def setup
+        @webhook = Webhook.new
+        @webhook.app_id = 'BrownShirt'
+        @webhook.source = 'sample'
+        @webhook.save!
+    end
+
+    test 'valid webhook' do 
+        assert @webhook.valid?
+    end
+
+    test 'invalid without app_id' do 
+        @webhook.app_id = nil
+        @webhook.valid?
+
+        assert_equal @webhook.errors[:app_id], ["can't be blank"]
+    end
+
+    test 'invalid without source' do 
+        @webhook.source = nil
+        @webhook.valid?
+
+        assert_equal @webhook.errors[:source], ["can't be blank"]
+    end
+end
