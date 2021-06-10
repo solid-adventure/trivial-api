aae6b35b6f743262754c47a047c4a550ee2ba7f2
Juan Carbajal <juancarbajal@Juans-Mac-mini.lan>
2021-06-10 15:31:08 -0700

    Preventing concurrency issue and more slight refactors

--- app/models/webhook.rb
+++ app/models/webhook.rb
@@ -6,45 +6,45 @@ class Webhook < ApplicationRecord
     belongs_to :user
     
     def self.chart_stats(app_id, number_of_days)
-        @_chart_stats = []
-        self.get_chart_stats(app_id,number_of_days)
-        self.format_chart_stats(number_of_days)
-        return @_chart_stats
+        return self.get_chart_stats(app_id,number_of_days)
     end
+
+    private 
 
     def self.get_chart_stats(app_id, number_of_days)
-        puts Time.now-number_of_days.days
-        @_stats = Webhook.where("app_id = ? AND created_at > ?", app_id, Time.now.utc-number_of_days.days).group("DATE_TRUNC('day', created_at)").group(:status).size
+        stats = Webhook.where("app_id = ? AND created_at > ?", app_id, number_of_days.days.ago).group("DATE_TRUNC('day', created_at)").group(:status).count(:all)
+        self.format_chart_stats(stats, number_of_days)
     end
 
-    def self.format_chart_stats(number_of_days)
+    def self.format_chart_stats(stats, number_of_days)
+        chart_stats = []
         (0..number_of_days-1).each {|i|
-            webhook_logged = self.webhook_by_date(i)
-            if webhook_logged
-                @_chart_stats.push({
-                    date: Time.parse(@_key.to_s).strftime("%m/%d/%Y"),
+            key = Time.parse((Date.today - i.days).to_s)
+            count = self.webhook_by_date(i, stats, key)
+            if count.empty?
+                chart_stats.push({
+                    date: Time.parse(key.to_s).strftime("%m/%d/%Y"),
                     count: {}
                 })
             else
                 count_concat = {}
-                @_count.keys.each do |c|
-                    count_concat[c.second] = @_count[c]
+                count.keys.each do |c|
+                    count_concat[c.second] = count[c]
                 end
-                @_chart_stats.push({
-                    date: Time.parse(@_key.to_s).strftime("%m/%d/%Y"),
+                chart_stats.push({
+                    date: Time.parse(key.to_s).strftime("%m/%d/%Y"),
                     count: count_concat
                 })
             end
         }
+        return chart_stats
     end
 
-    def self.webhook_by_date(index)
-        @_key = Time.parse((Date.today - index.days).to_s)
-        @_count = @_stats.select{ |w|
-            ws = Time.parse(w[0].to_s).strftime("%Y-%m-%d");
-            ks = Time.parse(@_key.to_s).strftime("%Y-%m-%d");
+    def self.webhook_by_date(index, stats, key)
+        count = stats.select{ |w|
+            ws = w[0].strftime("%Y-%m-%d");
+            ks = Time.parse(key.to_s).strftime("%Y-%m-%d");
             ws == ks
         }
-        @_count.empty? ? true : false
     end
 end
