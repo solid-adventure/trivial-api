e9d143a5b595ed26781dcc04d58f2c4a4c31ed34
Jeff Hunter <jeff@missioncriticallabs.com>
2021-09-30 19:42:47 -0400

    add created_at to user profile response and sort apps by descriptive name

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :role, :approval, :color_theme
+  attributes :id, :name, :email, :role, :approval, :color_theme, :created_at
 end
