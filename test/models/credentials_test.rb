f126e1872bbd6bd72c6c0d536f836c125eb7a712
Jeff Hunter <jeff@missioncriticallabs.com>
2022-03-08 15:21:08 -0500

    fix unstubbed AWS dependency in unit tests

--- test/models/credentials_test.rb
+++ test/models/credentials_test.rb
@@ -7,9 +7,11 @@ class CredentialsTest < ActiveSupport::TestCase
   end
 
   test 'grants permission to the app\'s role' do
-    Role.stub :create!, -> (n) { Role.new(name: n[:name], arn: "arn:x:#{n[:name]}") } do
-      assert_equal @app.aws_role,
-        @app.credentials.default_policy[:Statement].first[:Principal][:AWS]
+    Credentials.stub :find_by_app_and_name!, -> (a, n) { Credentials.new(app: a, name: n, arn: "arn:x:#{n}", secret_value: {}) } do
+      Role.stub :create!, -> (n) { Role.new(name: n[:name], arn: "arn:x:#{n[:name]}") } do
+        assert_equal @app.aws_role,
+          @app.credentials.default_policy[:Statement].first[:Principal][:AWS]
+      end
     end
   end
 end
