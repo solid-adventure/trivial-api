307a7f78703845c7102b070b5599875ce9b0ac30
David Richards <dpricha189@gmail.com>
2023-05-08 12:16:10 -0700

    Update insert endpoint to work without credentials

--- app/controllers/datastore_controller.rb
+++ app/controllers/datastore_controller.rb
@@ -12,12 +12,12 @@ class DatastoreController < ApplicationController
     def insert_values
         username = 'testing_user'
         records_inserted = DatastoreManager.verify_model_and_insert_records(
-            params[:records], 
-            params[:table_name], 
-            params[:unique_keys], 
-            current_user.email, 
-            current_user.id, 
-            current_user.role)
+            params[:records].to_json, 
+            params[:table_name].to_s, 
+            params[:unique_keys].to_s, 
+            username, 
+            132, 
+            'member')
         render json: {records_inserted: records_inserted}
     end
 end    
