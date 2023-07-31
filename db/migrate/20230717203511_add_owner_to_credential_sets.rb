d366af93fd52e910aa4f0a8ed231433b06eadee8
David Richards <dpricha189@gmail.com>
2023-07-31 09:55:05 -0700

    Remove reference

--- db/migrate/20230717203511_add_owner_to_credential_sets.rb
+++ db/migrate/20230717203511_add_owner_to_credential_sets.rb
@@ -2,6 +2,5 @@ class AddOwnerToCredentialSets < ActiveRecord::Migration[7.0]
   def change
     add_column :credential_sets, :owner_id, :bigint
     add_column :credential_sets, :owner_type, :string
-    add_reference :credential_sets, :owner, polymorphic: true, index: true
   end
 end
