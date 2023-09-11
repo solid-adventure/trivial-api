007d85e18976d10b93fe6e49f2a5522d61e6ecf1
James Marks <james.marks@withtrivial.com>
2023-09-11 11:43:28 -0700

    tests and cleanup

--- app/models/concerns/taggable.rb
+++ app/models/concerns/taggable.rb
@@ -15,21 +15,18 @@ module Taggable
     end
   end
 
-
- # tag_list = [{'currency': 'USD'}, {'customer_ids': '1'}]
-
   class_methods do
-   # {'currency': 'USD'}
-    def table_alias_for_tag(tag)
-      "#{tag.keys.first}_tags"
+
+    # App.find_by_all_tags([{'currency': 'USD'}, {'customer_ids': '1'}])
+    def find_by_all_tags(tag_list)
+      self.joins(joins_for_tags(tag_list)).where(where_for_tags(tag_list).join(" AND "))
     end
 
-    # joins = for_tags tag_list
     def joins_for_tags(tag_list)
       joins = []
       tag_list.each do |item|
         table_alias = table_alias_for_tag(item)
-        joins << <<-SQL
+        joins << <<~SQL
           LEFT OUTER JOIN "tags" #{table_alias}
           ON "#{table_alias}"."taggable_type" = 'App'
           AND "#{table_alias}"."taggable_id" = "apps"."id"
@@ -40,7 +37,6 @@ module Taggable
       return joins
     end
 
-    # where = where_for_tags(tag_list)
     def where_for_tags(tag_list)
       where = []
       tag_list.each do |item|
@@ -50,9 +46,8 @@ module Taggable
       return where
     end
 
-    # App.find_by_all_tags([{'currency': 'USD'}, {'customer_ids': '1'}])
-    def find_by_all_tags(tag_list)
-      self.joins(joins_for_tags(tag_list)).where(where_for_tags(tag_list).join(" AND "))
+    def table_alias_for_tag(tag)
+      "#{tag.keys.first}_tags"
     end
 
   end
