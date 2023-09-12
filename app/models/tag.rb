c71152520f5492b67da9075f2eadfff194659e83
James Marks <james.marks@withtrivial.com>
2023-09-12 10:07:58 -0700

    no ransack after all

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -7,13 +7,4 @@ class Tag < ApplicationRecord
   validates :name, uniqueness: { 
     scope: [:taggable_type, :taggable_id, :context] },
     strict: TagExists
-
-  def self.ransackable_attributes(auth_object = nil)
-    ["context", "name", "taggable_id", "taggable_type"]
-  end
-
-  def self.ransackable_associations(auth_object = nil)
-    ["taggable"]
-  end
-
 end
