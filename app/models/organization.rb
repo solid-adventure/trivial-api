8a6335f81795f85cb020133aae48d429dccd2a39
Jordyn Geiger <jordyn.geiger1@gmail.com>
2023-10-11 12:17:52 -0400

    resource alias

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,9 +1,9 @@
 class Organization < ApplicationRecord
-  has_many :apps, as: :owner
-  has_many :manifests, as: :owner
-  has_many :manifest_drafts, as: :owner
-  has_many :activity_entries, as: :owner
-  has_many :credential_sets, as: :owner
+  has_many :owned_apps, class_name: 'App', as: :owner
+  has_many :owned_manifests, class_name: 'Manifest', as: :owner
+  has_many :owned_manifest_drafts, class_name: 'ManifestDraft', as: :owner
+  has_many :owned_activity_entries, class_name: 'ActivityEntry', as: :owner
+  has_many :owned_credential_sets, class_name: 'CredentialSet', as: :owner
   has_many :org_roles, dependent: :delete_all
   has_many :users, through: :org_roles
 
