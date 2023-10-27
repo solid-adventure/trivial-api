4302082cacc77a7db78ade3d940225069e7e048e
Jordyn Geiger <jordyn.geiger1@gmail.com>
2023-10-27 12:38:19 -0400

    serializer fix

--- app/serializers/permission_serializer.rb
+++ app/serializers/permission_serializer.rb
@@ -1,7 +1,3 @@
 class PermissionSerializer < ActiveModel::Serializer
-  include JSONAPI::Serializer
-
-  set_type :permission
-
   attributes :id, :user_id, :permissible_id, :permissible_type, :permit
 end
