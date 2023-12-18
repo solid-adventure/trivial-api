b9a03ab3b60abf4db56f89f2a4592d1f93b08b79
Jordyn Geiger <jordyn.geiger1@gmail.com>
2023-12-18 17:01:30 -0500

    updated index_user, added permissions_for, updated test

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -8,6 +8,41 @@ class Permission < ApplicationRecord
   validates :permissible, presence: true
   validates :permit, presence: true
 
+  def self.permissions_for(user)
+    # we only care about special permissions. read and create are excluded
+    permissions = {
+      update: {},
+      destroy: {},
+      grant: {},
+      transfer: {}
+    }
+
+    # id apps by name
+    owned_apps = user.owned_apps.pluck(:name)
+    managed_apps = App.where(owner: user.organizations.where(org_roles: { role: "admin" })).pluck(:name)
+
+    # id credential_sets by id
+    owned_credentials = user.owned_credential_sets.pluck(:id)
+    managed_credentials = CredentialSet.where(owner: user.organizations.where(org_roles: { role: "admin" })).pluck(:id)
+
+    # build the resource permission arrays and return permissions
+    permissions.each do |ability, resources|
+      permission_conditions = { user: user, permit: PERMISSIONS_HASH[ability] }
+
+      permitted_app_ids = Permission
+        .where(permission_conditions.merge(permissible_type: "App"))
+        .pluck(:permissible_id)
+      permitted_apps = App.where(id: permitted_app_ids).pluck(:name)
+
+      permitted_credentials = Permission
+        .where(permission_conditions.merge(permissible_type: "CredentialSet"))
+        .pluck(:permissible_id)
+
+      resources[:app_names] = owned_apps + managed_apps + permitted_apps
+      resources[:credential_set_ids] = owned_credentials + managed_credentials + permitted_credentials
+    end
+  end
+
   def self.group_by_user(permissions)
     permissions = permissions.group_by(&:user_id)
 
