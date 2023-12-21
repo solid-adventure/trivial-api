b6ccf3299fcb0f7f62c5197771fe76a5537e677c
Jordyn Geiger <jordyn.geiger1@gmail.com>
2023-12-21 16:22:26 -0500

    comment and refinement per review

--- app/controllers/permissions_controller.rb
+++ app/controllers/permissions_controller.rb
@@ -75,9 +75,8 @@ class PermissionsController < ApplicationController
 
   private
     def authorize_transfer!
-      return true if @new_owner == current_user
-      return false unless @new_owner.is_a?(Organization) && current_user.organizations.exists?(@new_owner)
-      true
+      return true if @new_owner == current_user # This line is necessary for allowing a user to transfer from an Organization to themselves
+      @new_owner.is_a?(Organization) && current_user.organizations.exists?(@new_owner)
     end
 
     # Use callback to set the resource to determine permission actions
