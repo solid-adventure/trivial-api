a8a9dbe598128c5635dfb9c5be173db857ab1b16
Jordyn Geiger <jordyn.geiger1@gmail.com>
2024-01-03 11:37:18 -0500

    added auditing to models

--- app/models/org_role.rb
+++ app/models/org_role.rb
@@ -1,6 +1,8 @@
 class OrgRole < ApplicationRecord
   ROLES = %w(admin member guest).freeze
 
+  audited
+
   validates :user_id, presence: true
   validates :organization_id, presence: true
   validates :role, presence: true, inclusion: { in: ROLES }
