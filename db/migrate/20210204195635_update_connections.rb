eed8ba4bb8bd3fd2cdf889b0484a966ea0bf4734
James Marks <james.marks@withtrivial.com>
2024-01-05 09:23:25 -0800

    skip old rake task

--- db/migrate/20210204195635_update_connections.rb
+++ db/migrate/20210204195635_update_connections.rb
@@ -5,11 +5,6 @@ class UpdateConnections < ActiveRecord::Migration[6.0]
     add_column :connections, :from, :string
     add_column :connections, :to, :string
 
-    unless reverting?
-      Rake::Task['tasks:migrate_stages'].invoke
-      Rake::Task['tasks:import_from_to'].invoke
-    end
-
     remove_reference :connections, :from
     remove_reference :connections, :to
   end
