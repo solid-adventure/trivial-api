86ecc100e917abcff975892dfcf2ee2f1145d0dc
Jordyn Geiger <jordyn.geiger1@gmail.com>
2024-01-11 17:56:02 -0500

    fix for return types

--- app/models/user.rb
+++ app/models/user.rb
@@ -44,7 +44,7 @@ class User < ActiveRecord::Base
   before_create :set_trial_expires_at
 
   def ensure_aws_role!
-    return unless aws_env_set?
+    return "" unless aws_env_set?
     name = "#{ENV['AWS_ROLE_PREFIX'] || ''}lambda-ex-#{id.to_s(36)}"
     update!(aws_role: Role.create!(name: name).arn) if aws_role.blank?
     aws_role
