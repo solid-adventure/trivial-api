41ad2840e25633ede19fc4c985f0916c987e0f2a
Jordyn Geiger <jordyn.geiger1@gmail.com>
2024-01-17 16:17:03 -0500

    updated environment variable usage

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'mg.trivialapps.io',
+    :domain         => ENV['MAILGUN_DOMAIN'],
     :authentication => :plain,
     }
   ActionMailer::Base.delivery_method = :smtp
