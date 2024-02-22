2e21d7d766b9cf8396e6ad6cd726ba40c728a196
Jordyn Geiger <jordyn.geiger1@gmail.com>
2024-02-22 17:02:38 -0500

    seeds.rb creates register_items

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,34 +12,32 @@ admin = User.new
 admin.name = 'admin'
 admin.email = 'admin@email.com'
 admin.role = 'admin'
-admin.password = '12345678'
+admin.password = '@Password123'
 admin.approval = 'approved'
 admin.save!
+user = User.last
 
+income_accounts = %w(carrier_fees storage receiving shipping VAS)
+warehouses = %w(san_francisco los_angeles new_york paris london tokyo)
+channels = %w(retail wholesale online popup dropship)
+originated_at_range = ((DateTime.now - 2.years)..DateTime.now)
 
-# TODO This concept is sound and runs well in console, but should be verified before uncommeting officially
+register1 = Register.find_or_create_by(name: "Generated Register 1", owner: user, units: "USD", meta: {meta0: "income_account", meta1: "warehouse", meta2: "channel" })
+register2 = Register.find_or_create_by(name: "Generated Register 2", owner: user, units: "USD", meta: {meta0: "income_account", meta1: "warehouse", meta2: "channel" })
+register3 = Register.find_or_create_by(name: "Generated Register 3", owner: user, units: "USD", meta: {meta0: "income_account", meta1: "warehouse", meta2: "channel" })
+registers = [register1, register2, register3]
 
-# income_accounts = %w(carrier_fees storage receiving shipping VAS)
-# warehouses = %w(san_francisco los_angeles new_york paris london tokyo)
-# channels = %w(retail wholesale online popup dropship)
-# user = User.third
-
-# register1 = Register.find_or_create_by(name: "Generated Register 1", owner: user, units: "USD", meta: {meta0: "income_account", meta1: "warehouse", meta2: "channel" })
-# register2 = Register.find_or_create_by(name: "Generated Register 2", owner: user, units: "USD", meta: {meta0: "income_account", meta1: "warehouse", meta2: "channel" })
-# register3 = Register.find_or_create_by(name: "Generated Register 3", owner: user, units: "USD", meta: {meta0: "income_account", meta1: "warehouse", meta2: "channel" })
-# registers = [register1, register2, register3]
-
-# (1..1000000).map { |i| RegisterItem.create(
-#   register_id: registers.sample.id,
-#   owner: user,
-#   description: "Generated event #{i}",
-#   amount: rand(0.1..20.0).round(2),
-#   units: "USD",
-#   unique_key: "#{Time.now}-#{i}",
-#   income_account: income_accounts.sample,
-#   warehouse: warehouses.sample,
-#   channel: channels.sample
-# )
-# puts "Generated event #{i} of 1,000,000"
-# }
-
+2000.times do |i|
+  RegisterItem.create(
+    register_id: registers.sample.id,
+    owner: user,
+    description: "Generated event #{i}",
+    amount: rand(0.1..20.0).round(2),
+    units: "USD",
+    unique_key: "#{Time.now}-#{i}",
+    income_account: income_accounts.sample,
+    warehouse: warehouses.sample,
+    channel: channels.sample, 
+    originated_at: rand(originated_at_range)
+  )
+end
