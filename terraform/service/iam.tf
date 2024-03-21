c9395f5c492e8621d14d6939109adea959bedc7d
Ben Bowen <ben.bowen2@gmail.com>
2024-03-21 11:56:59 -0400

    better secrets

---
+++ terraform/service/iam.tf
@@ -0,0 +1,61 @@
+resource "aws_iam_role " "ecs_task_execution_role" {
+  name = "${var.service_name}-ecs-task-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_policy" "ecs_task_iam_policy" {
+  name        = "${var.service_name}-ecs-execution-policy"
+  path        = "/"
+  description = "${var.service_name} ECS Execution Policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "secretsmanager:GetSecretValue"
+        ]
+        Effect   = "Allow"
+        Resource = [
+          data.aws_secretsmanager_secret.secrets.arn,
+          "${data.aws_secretsmanager_secret.secrets.arn}:*"
+        ]
+      },
+      {
+        Action = [
+          "logs:PutLogEvents",
+          "logs:CreateLogStream",
+          "iam:PassRole",
+          "ecs:*",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability"
+        ]
+        Effect   = "Allow"
+        Resource = [
+          data.aws_secretsmanager_secret.secrets.arn,
+          "${data.aws_secretsmanager_secret.secrets.arn}:*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_ecs_task_policy_to_role" {
+  role =  aws_iam_role.ecs_task_execution_role.name
+  policy_arn = aws_iam_policy.ecs_task_iam_policy.arn
+}
--- terraform/service/secrets.tf
+++ terraform/service/secrets.tf
@@ -1,6 +1,6 @@
-#data "aws_secretsmanager_secret" "secrets" {
-#  name: "trivial-api"
-#}
+data "aws_secretsmanager_secret" "secrets" {
+  name = "trivial-api"
+}
 #
 #data "aws_secretsmanager_secret_version" "current" {
 #  secret_id = data.aws_secretsmanager_secret.secrets.id
