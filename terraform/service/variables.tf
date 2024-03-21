91833512595711f2a25ab40ee544b1c8baa05030
Ben Bowen <ben.bowen2@gmail.com>
2024-03-21 10:56:39 -0400

    better secrets

--- terraform/service/variables.tf
+++ terraform/service/variables.tf
@@ -7,20 +7,3 @@ variable "repo" {}
 variable "service_name" {}
 variable "ecr_repo" {}
 variable "image_tag" {}
-
-# Service Specific Variables. These should be set either for CI or in the vars files depending on sensitivity.
-variable "core_oauth_client_id" {
-  sensitive = true
-}
-variable "core_oauth_client_secret" {
-  sensitive = true
-}
-variable "mailgun_password" {
-  sensitive = true
-}
-variable "jwt_private_key" {
-  sensitive = true
-}
-variable "client_keys" {
-  sensitive = true
-}
