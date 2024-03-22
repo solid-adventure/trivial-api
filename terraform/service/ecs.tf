20b6aa6947350eb53d92cb3e10b8e928ea21ad5c
Ben Bowen <ben.bowen2@gmail.com>
2024-03-22 09:22:13 -0400

    plz just work ok

--- terraform/service/ecs.tf
+++ terraform/service/ecs.tf
@@ -87,138 +87,126 @@ locals {
   }
 
   task_definition = {
-      name      = "${local.name_prefix}-trivial-api"
-      image     = var.ecr_tag
-      cpu       = lookup(local.ecs_cpu, var.env, -1) - var.datadog_agent_cpu
-      memory    = lookup(local.ecs_mem, var.env, -1) - var.datadog_agent_memory
-      essential = true
-      firelensConfiguration : null,
-      logConfiguration : {
-        logDriver : "awsfirelens",
-        options : {
-          "dd_message_key" : "log",
-          "apikey" : var.datadog_api_key,
-          "provider" : "ecs",
-          "dd_source" : "aws",
-          "dd_service" : var.service_name,
-          "Host" : "http-intake.logs.datadoghq.com",
-          "dd_tags" : "env:${var.env},version:${local.datadog_version},ecs_service_name:${aws_ecs_service.trivial_api_task-service.name}",
-          "TLS" : "on",
-          "Name" : "datadog"
-        }
-      },
-      portMappings = [
-        {
-          containerPort = 3000
-          hostPort      = 3000
-        }
-      ]
-      secrets = [
-        {
-          "name" : "WHIPLASH_CLIENT_ID",
-          "valueFrom" : "${data.aws_secretsmanager_secret.trivial_api_secrets.arn}:whiplash_client_id::"
-        },
-        {
-          "name" : "WHIPLASH_CLIENT_SECRET",
-          "valueFrom" : "${data.aws_secretsmanager_secret.trivial_api_secrets.arn}:whiplash_client_secret::"
-        },
-        {
-          "name" : "CLIENT_SECRET",
-          "valueFrom" : "${data.aws_secretsmanager_secret.trivial_api_secrets.arn}:client_secret::"
-        },
-        {
-          "name" : "CLIENT_KEYS",
-          "valueFrom" : "${data.aws_secretsmanager_secret.trivial_api_secrets.arn}:client_keys::"
-        },
-        {
-          "name" : "MAILGUN_PASSWORD",
-          "valueFrom" : "${data.aws_secretsmanager_secret.trivial_api_secrets.arn}:mailgun_password::"
-        },
-      ]
-      environment = [
-        # TODO : remove once things are more stable, this env var should never go to prod
-        {
-          name : "SECRET_KEY_BASE",
-          value : "whatdowesetthistoo"
-        },
-        {
-          name : "RAILS_ENV",
-          value : "production"
-        },
-        {
-          name : "RAILS_LOG_TO_STDOUT",
-          value : "true"
-        },
-        {
-          name : "POSTGRES_HOST",
-          value : local.trivial_postgres.host
-        },
-        {
-          name : "POSTGRES_DATABASE",
-          value : local.trivial_postgres.db
-        },
-        {
-          name : "POSTGRES_USER",
-          value : local.trivial_postgres.user
-        },
-        {
-          name : "POSTGRES_PASSWORD",
-          value : local.trivial_postgres.password
-        },
-        {
-          name : "EXTERNAL_HOST",
-          value : local.trivial_api_service_domain
-        },
-        {
-          name : "MAILGUN_DOMAIN",
-          value : "whiplash.com"
-        },
-        {
-          name : "MAILGUN_SMTP_LOGIN",
-          value : "billingapp@mg.whiplash.com"
-        },
-        {
-          name : "MAILGUN_SMTP_PORT",
-          value : "587"
-        },
-        {
-          name : "MAILGUN_SMTP_SERVER",
-          value : "smtp.mailgun.org"
-        },
-        {
-          name : "DEFAULT_URL_HOST",
-          value : "https://${local.trivial_api_service_domain}"
-        },
-        {
-          name : "DEFAULT_URL_PORT",
-          value : "443"
-        },
-        {
-          name : "WHIPLASH_BASE_URL",
-          value : "https://${local.core_service_domain}"
-        },
-        {
-          name : "TRIVIAL_UI_URL",
-          value : local.trivial_ui_service_domain
-        },
-        {
-          name : "DD_ENV",
-          value : var.env
-        },
-        {
-          name : "DD_SERVICE",
-          value : var.service_name
-        },
-        {
-          name : "DD_VERSION",
-          value : local.datadog_version
-        }
-      ]
+    name      = "${local.name_prefix}-trivial-api"
+    image     = var.ecr_tag
+    cpu       = lookup(local.ecs_cpu, var.env, -1) - var.datadog_agent_cpu
+    memory    = lookup(local.ecs_mem, var.env, -1) - var.datadog_agent_memory
+    essential = true
+    firelensConfiguration : null
+    logConfiguration : {
+      logDriver : "awsfirelens"
+      options : {
+        "dd_message_key" : "log"
+        "apikey" : var.datadog_api_key
+        "provider" : "ecs"
+        "dd_source" : "aws"
+        "dd_service" : var.service_name
+        "Host" : "http-intake.logs.datadoghq.com"
+        "dd_tags" : "env:${var.env},version:${local.datadog_version},ecs_service_name:${local.name_prefix}-${var.service_name}"
+        "TLS" : "on"
+        "Name" : "datadog"
+      }
     }
+    portMappings = [
+      {
+        containerPort = 3000
+        hostPort      = 3000
+      }
+    ]
+    secrets = [
+      {
+        "name": "WHIPLASH_CLIENT_ID",
+        "valueFrom": "${data.aws_secretsmanager_secret.trivial_api_secrets.arn}:whiplash_client_id::"
+      },
+      {
+        "name": "WHIPLASH_CLIENT_SECRET",
+        "valueFrom": "${data.aws_secretsmanager_secret.trivial_api_secrets.arn}:whiplash_client_secret::"
+      },
+      {
+        "name": "CLIENT_SECRET",
+        "valueFrom": "${data.aws_secretsmanager_secret.trivial_api_secrets.arn}:client_secret::"
+      },
+      {
+        "name": "CLIENT_KEYS",
+        "valueFrom": "${data.aws_secretsmanager_secret.trivial_api_secrets.arn}:client_keys::"
+      },
+      {
+        "name": "MAILGUN_PASSWORD",
+        "valueFrom": "${data.aws_secretsmanager_secret.trivial_api_secrets.arn}:mailgun_password::"
+      },
+    ]
+    environment = [
+      # TODO : remove once things are more stable, this env var should never go to prod
+      {
+        name : "SECRET_KEY_BASE",
+        value : "whatdowesetthistoo"
+      },
+      {
+        name : "RAILS_ENV",
+        value : "production"
+      },
+      {
+        name : "RAILS_LOG_TO_STDOUT",
+        value : "true"
+      },
+      {
+        name : "POSTGRES_HOST",
+        value : local.trivial_postgres.host
+      },
+      {
+        name : "POSTGRES_DATABASE",
+        value : local.trivial_postgres.db
+      },
+      {
+        name : "POSTGRES_USER",
+        value : local.trivial_postgres.user
+      },
+      {
+        name : "POSTGRES_PASSWORD",
+        value : local.trivial_postgres.password
+      },
+      {
+        name : "EXTERNAL_HOST",
+        value : local.trivial_api_service_domain
+      },
+      {
+        name : "MAILGUN_DOMAIN",
+        value : "whiplash.com"
+      },
+      {
+        name : "MAILGUN_SMTP_LOGIN",
+        value : "billingapp@mg.whiplash.com"
+      },
+      {
+        name : "MAILGUN_SMTP_PORT",
+        value : "587"
+      },
+      {
+        name : "MAILGUN_SMTP_SERVER",
+        value : "smtp.mailgun.org"
+      },
+      {
+        name : "DEFAULT_URL_HOST",
+        value : "https://${local.trivial_api_service_domain}"
+      },
+      {
+        name : "DEFAULT_URL_PORT",
+        value : "443"
+      },
+      {
+        name : "WHIPLASH_BASE_URL",
+        value : "https://${local.core_service_domain}"
+      },
+      {
+        name: "TRIVIAL_UI_URL",
+        value: local.trivial_ui_service_domain
+      }
+    ]
+  }
 }
 
 resource "aws_ecs_service" "trivial_api_task-service" {
-  name                              = "${local.name_prefix}-trivial-api"
+  name                              = "${local.name_prefix}-${var.service_name}"
   cluster                           = local.ecs_cluster_id
   task_definition                   = aws_ecs_task_definition.trivial_api_task_definition.arn
   desired_count                     = lookup(local.desired_count, var.env, -1)
@@ -242,6 +230,8 @@ resource "aws_ecs_service" "trivial_api_task-service" {
 resource "aws_ecs_task_definition" "trivial_api_task_definition" {
   container_definitions = jsonencode(concat([
     local.task_definition,
+    local.agent_definition,
+    local.log_router_definition
   ]))
   family        = "${local.name_prefix}-trivial-api-task"
   network_mode  = "awsvpc"
