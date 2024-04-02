9338c49faa6c3b6233fc8d6ea9b20de7e37a5bd8
Ben Bowen <ben.bowen2@gmail.com>
2024-04-02 09:08:14 -0400

    adds internal sg

--- terraform/service/locals.tf
+++ terraform/service/locals.tf
@@ -33,11 +33,12 @@ locals {
   name_prefix            = data.terraform_remote_state.whiplash-regional.outputs.regional_prefix
   ecs_cluster_id         = data.terraform_remote_state.whiplash-regional.outputs.ecs_cluster_id
   ecs_security_group_ids = data.terraform_remote_state.whiplash-regional.outputs.ecs_security_group_ids
-  ecs_subnet_ids         = data.terraform_remote_state.whiplash-regional.outputs.private_subnet_ids
+  ecs_subnet_ids         = data.terraform_remote_state.whiplash-regional.outputs.internal_subnet_ids
   ecs_task_role_arn      = data.terraform_remote_state.whiplash-regional.outputs.ecs_task_role_arn
 
-  alb_target_group_arn = data.terraform_remote_state.trivial-infra.outputs.trivial_api_alb_target_group_arn
-  internal_alb_target_group_arn = data.terraform_remote_state.trivial-infra.outputs.internal_trivial_api_alb_target_group_arn
+  alb_target_group_arn                      = data.terraform_remote_state.trivial-infra.outputs.trivial_api_alb_target_group_arn
+  internal_alb_target_group_arn             = data.terraform_remote_state.trivial-infra.outputs.internal_trivial_api_alb_target_group_arn
+  allow_internal_vpc_traffic_security_group = data.terraform_remote_state.trivial-infra.outputs.allow_internal_vpc_traffic_security_group_arn
 
   trivial_postgres = {
     host     = data.terraform_remote_state.trivial-infra.outputs.trivial_database_host
