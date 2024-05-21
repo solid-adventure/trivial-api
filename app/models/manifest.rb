ee04abb806f7e8af8b98d2b96426e53c6ca1537e
Jordyn Geiger <jordyn.geiger1@gmail.com>
2024-05-21 14:18:56 -0400

    create activity_entry on manifest save

--- app/models/manifest.rb
+++ app/models/manifest.rb
@@ -14,6 +14,25 @@ class Manifest < ApplicationRecord
 
     has_one_attached :bundle
 
+    after_save :create_activity_entry
+
+    def create_activity_entry
+        current_audit = audits.last
+        entry = ActivityEntry.new
+        entry.owner = owner
+        entry.app = app
+        entry.activity_type = 'build'
+        entry.status = '200'
+        entry.diagnostics = {
+            build_info: {
+                audit_id: current_audit.id,
+                action: current_audit.action,
+                user: current_audit.user_id
+            }
+        }
+        entry.save!
+    end
+
     def copy_to_app!(new_app)
         new_manifest = self.dup
         new_manifest.app_id = new_app.name
