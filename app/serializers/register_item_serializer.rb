ad963f41af3038e5ee274b8c72e0a733448fc4ae
Jordyn Geiger <jordyn.geiger1@gmail.com>
2024-06-03 19:03:33 -0400

    remove alias methods for register_item meta-column
    moved alias and un-alias functionality to controller and serializer
    replaced register_item model tests with controller tests
    updated db/seeds to match current use patterns
    updated serializer to use rails native caching
    update/cleanup of rails fixtures

--- app/serializers/register_item_serializer.rb
+++ app/serializers/register_item_serializer.rb
@@ -1,18 +1,12 @@
 class RegisterItemSerializer < ActiveModel::Serializer
   attributes :id, :register_id, :owner_type, :owner_id, :unique_key, :description, :amount, :units, :originated_at, :created_at
 
-  @@register_metas = {}
-
   def attributes(*args)
     data = super
     return data unless object.register_id
-    
-    unless @@register_metas[object.register_id]
-      @@register_metas[object.register_id] = object.register.meta.symbolize_keys
-    end
-    
-    @@register_metas[object.register_id].values.each do |label|
-      data[label] = object.send(label)
+
+    Register.where(id: object.register_id).pick(:meta).each do |column, label|
+      data[label] = object[column]
     end
 
     data
