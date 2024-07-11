2cd1bd0e410c0865b13856d08e496883382161e3
Jordyn Geiger <jordyn.geiger1@gmail.com>
2024-07-11 16:08:12 -0400

    reinitialize after migrate

--- db/migrate/20240710205557_update_activity_entry_payload_keys_to_version_2.rb
+++ db/migrate/20240710205557_update_activity_entry_payload_keys_to_version_2.rb
@@ -4,5 +4,6 @@ class UpdateActivityEntryPayloadKeysToVersion2 < ActiveRecord::Migration[7.0]
       version: 2,
       revert_to_version: 1,
       materialized: true
+    ActivityEntryPayloadKeys.reset_column_information
   end
 end
