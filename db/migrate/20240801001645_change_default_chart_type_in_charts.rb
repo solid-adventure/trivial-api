3e03f8c565a1466cc6c61281bc53adb49d9be9a0
drowninginflowers <jordyn.geiger1@gmail.com>
2024-08-03 17:17:03 -0400

    chart-type default migration now reversible

--- db/migrate/20240801001645_change_default_chart_type_in_charts.rb
+++ db/migrate/20240801001645_change_default_chart_type_in_charts.rb
@@ -1,6 +1,6 @@
 class ChangeDefaultChartTypeInCharts < ActiveRecord::Migration[7.0]
   def change
-    change_column_default :charts, :chart_type, 'table'
+    change_column_default :charts, :chart_type, from: 'gross_revenue', to: 'table'
 
     Chart.where(chart_type: 'gross_revenue').update_all(chart_type: 'table')
   end
