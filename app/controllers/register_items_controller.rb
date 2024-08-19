56e53756b6ea4f6e2f3599b4080c34851082e37d
James Marks <james.marks@withtrivial.com>
2024-08-19 16:53:45 -0700

    actually stream response

--- app/controllers/register_items_controller.rb
+++ app/controllers/register_items_controller.rb
@@ -149,9 +149,8 @@ class RegisterItemsController < ApplicationController
   end
 
   def set_streaming_headers
-    #nginx doc: Setting this to "no" will allow unbuffered responses suitable for Comet and HTTP streaming applications
     headers['X-Accel-Buffering'] = 'no'
-
+    headers["Last-Modified"] = Time.now.httpdate.to_s
     headers["Cache-Control"] ||= "no-cache"
     headers.delete("Content-Length")
   end
