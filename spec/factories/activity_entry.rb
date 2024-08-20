012204bb62b5821fc6025592ee381755e8717f6c
Jordyn Geiger <jordyn.geiger1@gmail.com>
2024-08-20 19:40:21 -0400

    rake task to remove old activity_entries removes older unused task new task removes entries with no register item before set date adds tests for new task

--- spec/factories/activity_entry.rb
+++ spec/factories/activity_entry.rb
@@ -3,6 +3,25 @@ FactoryBot.define do
   factory :activity_entry do
     association :owner, factory: :user
     app { FactoryBot.create(:app, owner: owner) }
+    activity_type { 'test' }
+    status { '200' }
+    source { 'localhost' }
+    duration_ms { rand(200..5000) }
+    sequence :payload do |n|
+      {
+        key: "unique-key-#{n}",
+        event_name: "test-event-#{n}",
+        event: {
+          id: n,
+        }
+      }
+    end
+    diagnostics {
+      {
+        errors: [],
+        events: []
+      }
+    }
 
     trait :org_owner do
       association :owner, factory: :organization
