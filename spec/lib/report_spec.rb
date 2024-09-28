c3fc3fa04685c1e33dbffbb8c2ce51ee31fa6431
James Marks <james.marks@withtrivial.com>
2024-09-28 14:54:39 -0700

    split report into 2 date ranges and cache the older results
    cache key as public method for future deletion ability
    
    simpler tests + cache test
    
    entire result cached edge, less fussy gap between cache cutoff and cache expiration
    
    cutoff is timezone-specific
    
    timezone consistency in tests
    
    unify cutoff across requests to ever hit the cache
    
    cleanup

--- spec/lib/report_spec.rb
+++ spec/lib/report_spec.rb
@@ -43,6 +43,130 @@ RSpec.describe Services::Report do
       expect(results[:count].first[:value]).to eq(2)
     end
 
+    it 'only uses the cache if the entire period is before the cache cutoff' do
+      expect(report).to receive(:cached_simple_stat_lookup).once
+      report.send(:simple_stat_lookup, 'count', args)
+    end
+
+    it 'only uses the live query if the entire period is after the cache cutoff' do
+      args[:start_at] = (Time.now - 1.day).iso8601
+      args[:end_at] = Time.now.iso8601
+      expect(report).to receive(:_simple_stat_lookup).once
+      report.send(:simple_stat_lookup, 'count', args)
+    end
+
+    it 'splits date range into a live and a cached query' do
+      args[:timezone] = 'Etc/GMT+4'
+      args[:end_at] = Time.now.in_time_zone(args[:timezone]).iso8601
+
+      start_time = args[:start_at]
+      end_time = args[:end_at]
+      cutoff = report.cache_cutoff(args[:timezone]).iso8601
+
+      expect(report).to receive(:cached_simple_stat_lookup).once do |stat, options|
+        expect(stat).to eq('count')
+        expect(options[:start_at]).to eq(start_time)
+        expect(options[:end_at]).to eq(cutoff)
+      end
+
+      expect(report).to receive(:_simple_stat_lookup).once do |stat, options|
+        expect(stat).to eq('count')
+        expect(options[:start_at]).to eq(cutoff)
+        expect(options[:end_at]).to eq(end_time)
+      end
+
+      allow(report).to receive(:combine_results).and_return({})
+      results = report.send(:simple_stat_lookup, 'count', args)
+    end
+
+    it 'makes use of the cache' do
+      expect(Rails.cache).to receive(:fetch).with(
+        report.send(:cache_key, 'count', args),
+         {expires_in: 1.day}
+      ).and_yield
+      report.send(:cached_simple_stat_lookup, 'count', args)
+    end
+
+    it 'combines the results of a live and a cached query' do
+      args[:end_at] = Time.now.iso8601
+      initialResult = report.send(:simple_stat_lookup, 'count', args)
+      expect(initialResult[:count].first[:value]).to eq(2)
+      FactoryBot.create(
+        :register_item,
+        register:,
+        originated_at: Time.now - 1.hour
+      )
+      updatedResult = report.send(:simple_stat_lookup, 'count', args)
+      expect(updatedResult[:count].first[:value]).to eq(3)
+    end
+
+    it 'uses a compatible timezone when splitting the queries' do
+      timezone = report.send(:validate_timezone!, 'Etc/GMT+5')
+      start_at = (Time.now - 1.week).in_time_zone(timezone).iso8601
+      cutoff = report.cache_cutoff(timezone).iso8601
+      expect(report.send(:validate_time_range!, start_at, cutoff, timezone)).to eq([start_at, cutoff])
+    end
+
+    it 'combines results when live and cached query have returned the same groups' do
+      cached_result = {
+        title: 'Count by Entity Type',
+        count: [
+          { period: 'All', group: 'Order', value: 2 }
+        ]
+      }
+      live_result = {
+        title: 'Count by Entity Type',
+        count: [
+          { period: 'All', group: 'Order', value: 3 }
+        ]
+      }
+      result = report.send(:combine_results, cached_result, live_result)
+      expect(result[:title]).to eq('Count by Entity Type')
+      expect(result[:count].length).to eq(1)
+      expect(result[:count][0][:value]).to eq(5)
+    end
+
+    it 'combines results when live and cached query have returned different periods' do
+      cached_result = {:title=>"Count by Month", :count=>[{:period=>"Jan 2023", :group=>"All", :value=>0}, {:period=>"Feb 2023", :group=>"All", :value=>1} ]}
+      live_result =   {:title=>"Count by Month", :count=>[{:period=>"Mar 2023", :group=>"All", :value=>2}, {:period=>"Apr 2023", :group=>"All", :value=>3} ]}
+      result = report.send(:combine_results, cached_result, live_result)
+      result[:count].each_with_index do |group, i|
+        expect(group[:value]).to eq(i)
+      end
+    end
+
+    it 'combines results when live and cached query have returned overlapping groups' do
+      cached_result = {:title=>"Count by Month", :count=>[{:period=>"Jan 2023", :group=>"All", :value=>0}, {:period=>"Feb 2023", :group=>"All", :value=>1} ]}
+      live_result =   {:title=>"Count by Month", :count=>[{:period=>"Feb 2023", :group=>"All", :value=>2} ]}
+      result = report.send(:combine_results, cached_result, live_result)
+      expect(result[:count][0][:value]).to eq(0) # Jan 2023, 0
+      expect(result[:count][1][:value]).to eq(3) # Feb 2023, 1 + 2
+    end
+
+    it 'combines results when live and cached query have multidimensional groups' do
+      cached_result = {:title=>"Count by Entity Type, Entity Id", :count=>[{:period=>"All", :group=>["Order", "1"], :value=>0}, {:period=>"All", :group=>["Order", "2"], :value=>1}]}
+      live_result =   {:title=>"Count by Entity Type, Entity Id", :count=>[{:period=>"All", :group=>["Order", "1"], :value=>2}, {:period=>"All", :group=>["Order", "3"], :value=>3}]}
+      result = report.send(:combine_results, cached_result, live_result)
+      expect(result[:count][0][:value]).to eq(2) # Order 1, 0 + 2
+      expect(result[:count][1][:value]).to eq(1) # Order 2, 1
+      expect(result[:count][2][:value]).to eq(3) # Order 3, 3
+    end
+
+     it 'combines results when live and cached query have multidimensional groups and different periods' do
+      cached_result = {:title=>"Count by Quarter and Entity Type, Entity Id", :count=>[{:period=>"Q1 2023", :group=>["Order", "2"], :value=>0}, {:period=>"Q2 2023", :group=>["Order", "2"], :value=>1}]}
+      live_result =   {:title=>"Count by Quarter and Entity Type, Entity Id", :count=>[{:period=>"Q3 2023", :group=>["Order", "3"], :value=>2}, {:period=>"Q3 2023", :group=>["Order", "4"], :value=>4}]}
+      result = report.send(:combine_results, cached_result, live_result)
+      expect(result[:count][0][:value]).to eq(0) # Q1 2023, Order 2, 0
+      expect(result[:count][1][:value]).to eq(1) # Q2 2023, Order 2, 1
+      expect(result[:count][2][:value]).to eq(2) # Q3 2023, Order 3, 2
+      expect(result[:count][3][:value]).to eq(4) # Q3 2023, Order 4, 4
+    end
+
+    it 'produces a valid cache key' do
+      key = report.send(:cache_key, 'count', args)
+      expect(key).to eq("simple_stat_lookup/count_{:start_at=>\"2023-01-01T00:00:00-04:00\", :end_at=>\"2023-12-31T23:59:59-04:00\", :register_id=>#{args[:register_id]}}")
+    end
+
     it 'only includes specified dates' do
       args[:end_at] = (start_at + 2.months - 1.second).iso8601 # Feb 28, 2023 11:59:59PM EDT
       results = report.send(:simple_stat_lookup, 'count', args)
