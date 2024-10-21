d970402c208f051de4131749d9608f5d5f958f4c
James Marks <james.marks@withtrivial.com>
2024-10-21 11:04:54 -0700

    comma comma

--- app/controllers/audits_controller.rb
+++ app/controllers/audits_controller.rb
@@ -90,7 +90,7 @@ class AuditsController < ApplicationController
           elsif audit.auditable_type == 'User' && key == 'tokens'
             {
               attribute: key,
-              patch: "Log In / Out"
+              patch: "Log In / Out",
               old_value: value.is_a?(Array) ? value[0] : nil,
               new_value: value.is_a?(Array) ? value[1] : value
             }
