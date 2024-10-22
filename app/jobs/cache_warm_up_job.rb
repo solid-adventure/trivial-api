8831f1bf7fce3245c67cd6fd0b97f4635557b5ce
Jordyn Geiger <116381228+drowninginflowers@users.noreply.github.com>
2024-10-22 15:06:31 -0400

    enhance/cache_warmup_job (#318)
    * clarity enhancements on cache warmup job
    
    * use `delay_duration` instead of `sleep` for sleep time
    
    * preface log outputs with `CacheWarmUpJob`
    
    * use `puts` instead of `Rails.logger` for partially stood up environments
    
    * add safety check to `Dashboards` migration
    
    * fixes for tests

--- app/jobs/cache_warm_up_job.rb
+++ app/jobs/cache_warm_up_job.rb
@@ -7,27 +7,28 @@ class CacheWarmUpJob < ApplicationJob
     when 'app_activity_stats'
       warm_up_app_activity_stats(**options)
     else
-      Rails.logger.info("Invalid cache_name: #{cache_name}. Exiting.")
+      puts("CacheWarmUpJob invalid cache_name: #{cache_name}. Exiting.")
       return
     end
 
-    Rails.logger.info("Cache warm up successful. Exiting.")
+    puts("CacheWarmUpJob successful. Exiting.")
   rescue Timeout::Error
-    Rails.logger.error("CacheWarmupJob timed out after 2 hours, cache: #{cache}")
+    puts("CacheWarmUpJob timed out after 2 hours, cache: #{cache}")
   rescue => e
-    Rails.logger.error("CacheWarmUpJob failed: #{e.message}")
+    puts("CacheWarmUpJob failed: #{e.message}")
   end
 
   private
 
-  def warm_up_app_activity_stats(app_ids: App.kept.pluck(:id), date_cutoff: Date.today - 7.days, sleep: rand(0..900))
-    raise 'app_ids must be an array of integers' unless app_ids.is_a? Array
-    raise 'date_cutoff must be a Date type' unless date_cutoff.is_a? Date
+  def warm_up_app_activity_stats(app_ids: App.kept.pluck(:id), date_cutoff: Date.today - 7.days, delay_duration: rand(0..900))
+    raise 'app_ids for app_activity_stats must be an array of integers' unless app_ids.is_a? Array
+    raise 'date_cutoff for app_activity_stats must be a Date type' unless date_cutoff.is_a? Date
 
-    sleep(sleep) # sleep to decrease DB collisions on multiple Rails instances
+    puts("CacheWarmUpJob Warm up app_activity_stats will start after #{delay_duration} seconds") if delay_duration > 0
+    sleep(delay_duration) # sleep to decrease DB collisions on multiple Rails instances
 
     Timeout.timeout(2.hours) do
-      Rails.logger.info("Warming up app_activity_stats cache with #{app_ids.size} apps and cutoff date #{date_cutoff}")
+      puts("CacheWarmUpJob Warming up app_activity_stats cache with #{app_ids.size} apps and cutoff date #{date_cutoff}")
       App.cache_stats_for!(app_ids:, date_cutoff:)
     end
   end
