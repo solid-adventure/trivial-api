729ddb5c019526b578932a910868a87ed0a1aff4
James Marks <james.marks@withtrivial.com>
2024-10-22 15:01:41 -0700

    cache warmup on startup as background job

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,5 +5,8 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /usr/src/app/tmp/pids/server.pid
 
+# Start atd daemon
+atd
+
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
