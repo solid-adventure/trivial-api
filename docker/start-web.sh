a884ff683f22674b58bd451fae8e78678884523a
James Marks <james.marks@withtrivial.com>
2024-10-23 13:32:51 -0700

    startup task warning

--- docker/start-web.sh
+++ docker/start-web.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# IMPORTANT Any tasks added here must complete in less than 30 seconds.
+#           If you add a task that does not complete in that time frame
+#           it will cause the container to be restarted in a loop.
+#
+#           Tasks that cannot complete in that timeframe must be run in the background.
+
 rails db:migrate
 rails server -b 0.0.0.0
