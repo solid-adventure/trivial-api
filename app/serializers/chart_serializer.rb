24c003194d1e2a4c3f321a5dcc5a40d9c7dae023
Jordyn Geiger <jordyn.geiger1@gmail.com>
2024-10-28 17:29:47 -0400

    added time defaults to controller, fix serializer

--- app/serializers/chart_serializer.rb
+++ app/serializers/chart_serializer.rb
@@ -1,7 +1,11 @@
 class ChartSerializer < ActiveModel::Serializer
-  attributes :id, :dashboard_id, :register_id, :name, :chart_type, :color_scheme, :invert_sign, :report_period, :default_timezone, :default_time_range, :report_groups
+  attributes :id, :dashboard_id, :register_id, :name, :chart_type, :color_scheme, :invert_sign, :report_period, :default_timezones, :default_time_range, :time_range_bounds, :report_groups
 
   def report_groups
-    object.aliased_groups()
+    object.aliased_groups
+  end
+
+  def time_range_bounds
+    object.time_range_bounds
   end
 end
