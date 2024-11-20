fb487dcba329979f8e9be11ca27b1b5ebda4e6f0
James Marks <James.marks@withtrivial.com>
2024-11-19 17:21:18 -0800

    Feature/activity search (#336)
    * activity search, search by IN
    
    * very high results limit
    
    * revert out of scope fix

--- app/controllers/activity_entries_controller.rb
+++ app/controllers/activity_entries_controller.rb
@@ -1,6 +1,6 @@
 class ActivityEntriesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:update, :create_from_request]
-  before_action :set_activity_entries, only: %i[index]
+  before_action :set_activity_entries, only: %i[index search]
 
   def index
     authorize! :index, ActivityEntry
@@ -20,6 +20,23 @@ class ActivityEntriesController < ApplicationController
     @activity_entries = @activity_entries.order(id: :desc).limit(limit)
     render json: @activity_entries, status: :ok, adapter: :attributes, each_serializer: ActivityEntryIndexSerializer
   rescue StandardError => exception
+    render_errors(exception, status: :unprocessable_entity)
+  end
+
+  def search
+    authorize! :index, ActivityEntry
+    search = if params[:search].is_a?(String)
+      JSON.parse(params[:search])
+    elsif params[:search].is_a?(Array)
+      params[:search]
+    end
+    raise 'search required' unless search.any?
+
+    @activity_entries = ActivityEntry.search(@activity_entries, search).limit(search_result_limit).order(id: :desc)
+
+    render json: @activity_entries, status: :ok , adapter: :attributes, each_serializer: ActivityEntryMemberSerializer
+  rescue StandardError => exception
+    Rails.logger.error "Search failed: #{exception.message}"
     render_errors(exception, status: :unprocessable_entity)
   end
 
@@ -142,7 +159,14 @@ class ActivityEntriesController < ApplicationController
   end
 
   private
+  # These can likely be unified in a future pass
+  # The 20 limit is likely much too low after a misdiagnosed missing index
   MAX_RESULTS = 20
+  MAX_SEARCH_RESULTS = 1000
+
+  def search_result_limit
+    [[(params[:limit] || MAX_SEARCH_RESULTS).to_i, 1].max, MAX_SEARCH_RESULTS].min
+  end
 
   def limit
     [[(params[:limit] || MAX_RESULTS).to_i, 1].max, MAX_RESULTS].min
