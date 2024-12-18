21cede01af5ca5947f0361045c598f53f86b63b7
James Marks <james.marks@withtrivial.com>
2024-12-17 17:45:31 -0800

    uniq list of customer ids for tags

--- app/jobs/create_orgs_from_tags_job.rb
+++ app/jobs/create_orgs_from_tags_job.rb
@@ -5,7 +5,7 @@ class CreateOrgsFromTagsJob < ApplicationJob
   def perform(options: {})
     contract_tags = Tag.where(context: 'customer_id', taggable_type: 'App')
     organization_tags = Tag.where(context: 'customer_id', taggable_type: 'Organization')
-    customer_ids = contract_tags.where.not(name: organization_tags.pluck(:name)).pluck(:name)
+    customer_ids = contract_tags.where.not(name: organization_tags.pluck(:name)).pluck(:name).uniq
     customer_ids.each do |customer_id|
       Organization.create_by_customer_id(customer_id)
     end
