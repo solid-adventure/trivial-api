814042f0d94a0c4a182fcee49044732f7236642a
Jordyn Geiger <jordyn.geiger1@gmail.com>
2024-12-17 20:28:55 -0500

    test fix

--- spec/jobs/create_orgs_from_tags_job_spec.rb
+++ spec/jobs/create_orgs_from_tags_job_spec.rb
@@ -9,22 +9,22 @@ RSpec.describe CreateOrgsFromTagsJob, type: :job do
     apps.each_with_index do |app, index|
       app.addTag!(:customer_id, index)
     end
-
-    Organization.create_by_customer_id(0)
   end
 
-  it 'creates an organization for each unassociated customer_id' do
-    expect(Tag.where(context: 'customer_id', taggable_type: 'App').count).to eq(5)
-    expect(Organization.count).to eq(1) # Only one organization created initially
-    expect(Tag.where(context: 'customer_id', taggable_type: 'Organization').count).to eq(1)
+  context 'when no customer_ids already have associated organizations' do
+    it 'creates an organization for each unassociated customer_id' do
+      expect(Tag.where(context: 'customer_id', taggable_type: 'App').count).to eq(5)
+      expect(Organization.count).to eq(0)
+      expect(Tag.where(context: 'customer_id', taggable_type: 'Organization').count).to eq(0)
 
-    expect {
-      job_instance.perform
-    }.to change {
-      Organization.count
-    }.from(1).to(5)
+      expect {
+        job_instance.perform
+      }.to change {
+        Organization.count
+      }.from(0).to(5)
 
-    expect(Tag.where(context: 'customer_id', taggable_type: 'Organization').count).to eq(5)
+      expect(Tag.where(context: 'customer_id', taggable_type: 'Organization').count).to eq(5)
+    end
   end
 
   context 'when all customer_ids already have associated organizations' do
